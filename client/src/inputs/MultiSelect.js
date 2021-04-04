4fe2ff27109192e52588999924a48891efb21289
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-03 21:57:06 -0500

    added lesson info

---
+++ client/src/inputs/MultiSelect.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { FormControl, InputLabel, Select, Input, Chip, MenuItem } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+
+const monthsList = [
+  "January",
+  "Feburary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "Sepetember",
+  "October",
+  "November",
+  "December",
+];
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const useStyles = makeStyles(() => ({
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: 2,
+  },
+}));
+
+const MultiSelect = ({ selectValue, handleMultiSelect }) => {
+  const classes = useStyles();
+
+  return (
+    <FormControl variant="outlined" fullWidth>
+      <InputLabel id="months-label">Invoice Month(s)</InputLabel>
+      <Select
+        required
+        labelId="months-label"
+        id="months-select"
+        multiple
+        value={selectValue}
+        onChange={handleMultiSelect}
+        input={<Input id="month-chip" />}
+        renderValue={(selected) => (
+          <div className={classes.chips}>
+            {selected.map((value) => (
+              <Chip key={value} label={value} className={classes.chip} />
+            ))}
+          </div>
+        )}
+        MenuProps={MenuProps}
+      >
+        {monthsList.map((month) => (
+          <MenuItem key={month} value={month}>
+            {month}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default MultiSelect;
--- client/src/pages/InvoicePage.js
+++ client/src/pages/InvoicePage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import DateInput from "../inputs/DataInput";
 import axios from "axios";
 import {
   FormControl,
@@ -9,6 +8,9 @@ import {
   TextField,
 } from "@material-ui/core";
 
+import DateInput from "../inputs/DataInput";
+import MultiSelect from "../inputs/MultiSelect";
+
 const InvoicePage = (props) => {
   const [userStudents, setUserStudents] = useState("");
 
@@ -16,19 +18,21 @@ const InvoicePage = (props) => {
   const [yourName, setYourName] = useState("");
   const [yourEmail, setYourEmail] = useState("");
   const [yourNumber, setYourNumber] = useState("");
-  const [months, setMonths] = useState([]);
 
   const [studentName, setStudentName] = useState("");
   const [parentName, setParentName] = useState("");
   const [parentEmail, setParentEmail] = useState("");
   const [parentPhone, setParentPhone] = useState("");
 
+  const [months, setMonths] = useState([]);
+  const [numLessons, setNumLessons] = useState(4);
+  const [lessons, setLessons] = useState([]);
+
   const getUserStudents = () => {
     axios
       .get(`/api/students?userid=${props.userId}`)
       .then((response) => {
         setUserStudents(response.data.data);
-        console.log(response);
       })
       .catch((error) => {
         console.log(error);
@@ -43,11 +47,39 @@ const InvoicePage = (props) => {
         setYourName(user.name);
         setYourEmail(user.email);
         setYourNumber(user.phoneNumber);
-        console.log(response);
       })
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  const handleMultiSelect = (event) => {
+    setMonths(event.target.value);
+  };
+
+  const createLessonState = (numberOfLessons) => {
+    const lessonState = [];
+    for (let i = 0; i < numberOfLessons; i++) {
+      lessonState.push({ date: new Date(), cost: 21 });
+    }
+    setLessons(lessonState);
+  };
+
+  const onCostChange = (event, inputId) => {
+    let numRegex = /[0-9]+/;
+    let result = inputId.match(numRegex);
+    updateFieldChanged("cost", Number(result[0]), Number(event.target.value));
+  };
+
+  const updateFieldChanged = (name, index, value) => {
+    let newArr = lessons.map((item, i) => {
+      if (index === i) {
+        return { ...item, [name]: value };
+      } else {
+        return item;
+      }
+    });
+    setLessons(newArr);
   };
 
   const onStudentSelect = (event) => {
@@ -71,6 +103,13 @@ const InvoicePage = (props) => {
       getUserStudents();
     }
   }, [props.userId]);
+
+  useEffect(() => {
+    if (numLessons && numLessons > 0) {
+      console.log("this ran");
+      createLessonState(numLessons);
+    }
+  }, [numLessons]);
 
   return (
     <div className="mt-4">
@@ -189,10 +228,47 @@ const InvoicePage = (props) => {
               </div>
             </div>
             <div className="row">
-              <div className="col">
-                <DateInput />
+              <div className="col mb-3">
+                <MultiSelect
+                  selectValue={months}
+                  handleMultiSelect={handleMultiSelect}
+                />
+              </div>
+              <div className="col mb-3">
+                <TextField
+                  required
+                  fullWidth
+                  type="number"
+                  id="lesson-number-input"
+                  label="Number of Lessons"
+                  variant="outlined"
+                  value={numLessons}
+                  onChange={(e) => setNumLessons(e.target.value)}
+                />
               </div>
             </div>
+
+            {lessons.length > 0 && (
+              <div className="row">
+                {console.log(lessons)}
+                <div className="col mb-3">
+                  <DateInput />
+                </div>
+                <div className="col mb-3">
+                  <TextField
+                    fullWidth
+                    id="cost-input-0"
+                    label="Amount"
+                    type="number"
+                    value={lessons[0].cost}
+                    variant="outlined"
+                    onChange={(e) => {
+                      onCostChange(e, e.target.id);
+                    }}
+                  />
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div className="col-lg-6 col-12"></div>
